99052f65d4fcb2792205bcb2cf6bfcb201c1ed57
AnuragKharwar <anurag.singh.tmk@gmail.com>
2025-01-24 15:52:24 +0530

    changes gradient

--- components/ui/ProjectCard.js
+++ components/ui/ProjectCard.js
@@ -86,7 +86,7 @@ function HoverPattern({ mouseX, mouseY, ...gridProps }) {
   return (
     <div className="pointer-events-none">
       <motion.div
-        className="absolute inset-0 rounded-xl bg-gradient-to-r from-blue-100 to-teal-100/50 opacity-0 transition duration-300 group-hover:opacity-100 dark:from-[#202D2E] dark:to-[#303428]"
+        className="absolute inset-0 rounded-xl bg-gradient-to-r from-lime-100 to-orange-100 opacity-0 transition duration-300 group-hover:opacity-100 dark:from-[#202D2E] dark:to-[#303428]"
         style={style}
       />
       <motion.div
